8b6ccd0c5a64ee7fd2614dc78618df20e84cc308
Seaony <seaony@outlook.com>
2026-08-31 09:18:46 +0800

    chore: initialize Yomi macOS project

---
+++ Yomi/YomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomiApp.swift
+//  Yomi
+//
+//  Created by seaony on 2026/8/31.
+//
+
+import SwiftUI
+
+@main
+struct YomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
